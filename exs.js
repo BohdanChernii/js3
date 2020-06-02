daadeb8eb396cfa37e06546111c75ad13f3bf135
promius <64136178+BohdanChernii@users.noreply.github.com>
2020-06-02 18:53:17 +0300

    Create exs.js

---
+++ exs.js
@@ -0,0 +1,7 @@
+first:https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript?fbclid=IwAR1UfrOWsMVDjqAt46HDJSCwpifZ-Hl1Cni0OoBejRk1_c9EQ8QQyO-6QFg
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+      return Math.min(...args);
+  }
+}
